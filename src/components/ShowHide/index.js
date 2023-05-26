c71cb3795bd58d19f6126967444e4979bad4e3a0
ramsai24 <jnrsai24@gmail.com>
2023-05-26 17:06:41 +0000

    4 test cases are pending

---
+++ src/components/ShowHide/index.js
@@ -0,0 +1,53 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class ShowHide extends Component {
+  state = {status1: true, status2: true}
+
+  changeMode1 = () => {
+    this.setState(preState => ({
+      status1: !preState.status1,
+    }))
+  }
+
+  changeMode2 = () => {
+    this.setState(preState => ({
+      status2: !preState.status2,
+    }))
+  }
+
+  render() {
+    const {status1, status2} = this.state
+    return (
+      <div className="bg-container">
+        <h1>Show/Hide</h1>
+        <div className="btns">
+          <div>
+            <div className="btn-container">
+              <button type="button" onClick={this.changeMode1}>
+                Show/Hide FirstName
+              </button>
+            </div>
+            <p className="btn-container1">
+              {status1 ? <p className="name">Joe</p> : null}
+            </p>
+          </div>
+
+          <div>
+            <div className="btn-container">
+              <button type="button" onClick={this.changeMode2}>
+                Show/Hide LastName
+              </button>
+            </div>
+            <p className="btn-container1">
+              {status2 ? <p className="name">Jonas</p> : null}
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ShowHide
